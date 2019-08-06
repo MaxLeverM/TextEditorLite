456f546e295777a280a06358bf562334c85e5d1c
teampromax@gmail.com <teampromax@gmail.com>
2019-08-06 15:37:40 +0300

    Deleted RichTextBox Added FastColoredTextBox

--- TextEditorLite/MainWindow.xaml.cs
+++ TextEditorLite/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Markup;
 
 namespace TextEditorLite
 {
@@ -34,14 +24,9 @@ namespace TextEditorLite
             if (loadWindow.ShowDialog() == true)
             {
                 textFile = loadWindow.filesListBox.SelectedItem as TextFile;
-                mainTextBox.SetText(textFile.Value);
+                mainTextBox.Text = textFile.Value;
                 textIsModified = false;
             }
-        }
-
-        private void MainTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            textIsModified = true;
         }
 
         private void NewBtn_Click(object sender, RoutedEventArgs e)
@@ -50,26 +35,20 @@ namespace TextEditorLite
             {*/
                 textIsModified = false;
                 textFile = null;
-                mainTextBox.Document.Blocks.Clear();
+                mainTextBox.Clear();
            /* }
             else
             {
 
             }*/
         }
-    }
 
-    public static class ExtRichTextBox
-    {
-        public static void SetText(this RichTextBox richTextBox, string text)
+        private void MainTextBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
-            richTextBox.Document.Blocks.Clear();
-            richTextBox.Document.Blocks.Add(new Paragraph(new Run(text)));
-        }
-
-        public static string GetText(this RichTextBox richTextBox)
-        {
-            return new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+            if (!textIsModified)
+            {
+                textIsModified = true;
+            }
         }
     }
 }
